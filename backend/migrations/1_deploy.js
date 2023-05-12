43dd5c72780b0579a4ec06e15f876133cfcc17d9
sundayonah <sundayonag94@gmail.com>
2023-05-12 06:08:04 +0100

    build and deployed

--- backend/migrations/1_deploy.js
+++ backend/migrations/1_deploy.js
@@ -1,4 +1,5 @@
 const Lottery = artifacts.require("Lottery")
 module.exports = function (deployer) {
-    deployer.deploy(Lottery);
+    deployer.deploy(Lottery)
 }
+//0x44D9639342223b2C4d502808cD5968eA22F88A6A CONTRACT ADDRESS
